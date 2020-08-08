1204684c3706e215e35b2abe9f70e41ee2d9a925
Frida <velia.arizpe@gmail.com>
2020-08-08 18:49:22 -0500

    fixed dates

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 $("#CitySearch").click(city)
 
+//dates
+var timedate = $("#timedate").html("("+(moment().format('l')+")"));
+var newdate =(new Date());
+var month = parseInt(newdate.getMonth()+1);
+var utcdate=parseInt(newdate.getUTCDate());
+var fullyear=parseInt(newdate.getFullYear());
+count=0;
+//onclick API Grab
 function city(){
     event.preventDefault;
+    console.log(month)
     var getcity = $("#entercity").val()
+    $("#Citydate").html(getcity)
     var apikey = "a27727791952336f9341e78353fcabb3"
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + getcity + "&appid=" + apikey
     var lat =""
@@ -13,31 +23,35 @@ function city(){
         method: "GET",
         async: true
       }).then(function(response) {
-        wait=true
-        //console.log(response);
         lat = (response.city.coord.lat)
         lon = (response.city.coord.lon)
-
-       
     var weatherurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon="+ lon +"&appid=" + apikey
-
         $.ajax({
             url: weatherurl,
             method: "GET",
-            async: true
         }).then(function(response) {
-            console.log(response);
             var ftoc= ((response.current.temp) - 273.15) * (9/5) + 32
-            $("#humidity").html(response.current.humidity)
-            $("#temp").html(ftoc)
-            
-        })
-     
+            var round = Math.round(10*ftoc)/10;     // round X to tenths
+            $("#humidity").html(response.current.humidity);
+            $("#temp").html(round);
+            $("#wind").html(response.current.wind_speed);
+            $("#uv").html(response.current.uvi);
+            console.log(month)
+            datos();    
+            function datos(){
+                $(".mb-0").each(function(){
+                   count++
+                  $(this).html(month +"/"+(utcdate+count)+"/" + fullyear)
+                })
 
+            }  
+        })
     });
-    
-    
 }
+
+
+
+
 
 //do calcs for rest of them
 //five separate  cards wit hdiffernt ids 
