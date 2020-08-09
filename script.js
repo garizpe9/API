f825a4f1923b06fde1b4de0b58273c38ce3c44a3
Frida <velia.arizpe@gmail.com>
2020-08-09 10:24:03 -0500

    temp and humidity in blue

--- script.js
+++ script.js
@@ -17,11 +17,9 @@ function city(){
     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + getcity + "&appid=" + apikey
     var lat =""
     var lon =""
-    var wait = false
     $.ajax({
         url: queryURL,
         method: "GET",
-        async: true
       }).then(function(response) {
         lat = (response.city.coord.lat)
         lon = (response.city.coord.lon)
@@ -30,20 +28,24 @@ function city(){
             url: weatherurl,
             method: "GET",
         }).then(function(response) {
-            var ftoc= ((response.current.temp) - 273.15) * (9/5) + 32
+            var ftoc= ((response.current.temp) - 273.15) * (9/5) + 32;
             var round = Math.round(10*ftoc)/10;     // round X to tenths
             $("#humidity").html(response.current.humidity);
             $("#temp").html(round);
             $("#wind").html(response.current.wind_speed);
             $("#uv").html(response.current.uvi);
-            console.log(month)
             datos();    
             function datos(){
                 $(".mb-0").each(function(){
-                   count++
-                  $(this).html(month +"/"+(utcdate+count)+"/" + fullyear)
+                  $(this).html(month +"/"+(utcdate+count)+"/" + fullyear)                
+                  var sm = $("<small>");
+                  $(this).append("<div>");
+                  $(this).append(sm);
+                  var temcon= ((response.daily[count].temp.max) - 273.15) * (9/5) + 32
+                  temround = Math.round(10*temcon)/10;
+                  sm.html("Temp: " + temround+"°F"+"<br>"+"Humidity: "+response.daily[count].humidity+"%");                 
+                  count++
                 })
-
             }  
         })
     });
@@ -54,8 +56,7 @@ function city(){
 
 
 //do calcs for rest of them
-//five separate  cards wit hdiffernt ids 
-//use daily array and append
+
 //on click function save the city name with unique id (maybe length of local storage)
 //loop through local storage after every enter button 
 //as well as beginning of rendering so every local storgae has a button with name of city in it
