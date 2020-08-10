17c9c83035e898dbf6a3060c97cf0bc98e861463
Frida <velia.arizpe@gmail.com>
2020-08-10 17:53:25 -0500

    half of the links

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $("#CitySearch").click(city)
 
+$(".addcity").click(newcity)
+
+
 //dates
 var timedate = $("#timedate").html("("+(moment().format('l')+")"));
 var newdate =(new Date());
@@ -12,7 +15,9 @@ idcount = localStorage.length;
 //Local Storage
 for (var i = 0; i < localStorage.length; i++) {
     $(".seecity").append("<tr>").append(`<td id='${i+1}'`)
+    //$(".seecity").append("<tr>").append(`<td id='${i+1}'`)
     $("#"+(i+1)).html(localStorage.getItem(i+1))
+    $("#"+(i+1)).click(oldcity)
 }
 
 //onclick API Grab
@@ -82,9 +87,60 @@ function city(){
     });
 }
 
-//do calcs for rest of them
+function oldcity(){
+    event.preventDefault;
+    var getcity = $(this).html()
+    $("#Citydate").html(getcity)
+    //API
+    var apikey = "a27727791952336f9341e78353fcabb3"
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + getcity + "&appid=" + apikey
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function(response) {
+        lat = (response.city.coord.lat)
+        lon = (response.city.coord.lon)
+    var weatherurl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat +"&lon="+ lon +"&appid=" + apikey
+        $.ajax({
+            url: weatherurl,
+            method: "GET",
+        }).then(function(response) {
+            //get t,h,w,u to main page with city
+            var ftoc= ((response.current.temp) - 273.15) * (9/5) + 32;
+            var round = Math.round(10*ftoc)/10;     // round X to tenths
+            $("#humidity").html(response.current.humidity);
+            $("#temp").html(round);
+            $("#wind").html(response.current.wind_speed);
+            $("#uv").html(response.current.uvi);
+             //bring icon to current weather
+            var iconcode = response.current.weather[0].icon;
+            var weathersite="http://openweathermap.org/img/wn/"+iconcode+"@2x.png";
+            $("#code").attr("src",weathersite);
+            //five day forecast
+            datos();    
+            function datos(){
+                counter=1
+                $(".mb-0").each(function(){
+                  $(this).html(month +"/"+(utcdate+counter)+"/" + fullyear)
+                  //images
+                  var lateweather=response.daily[counter].weather[0].icon;
+                  var a = $("<img>");
+                  a.attr("src","http://openweathermap.org/img/wn/"+lateweather+"@2x.png");
+                  $(this).append(a);
+                  //temp and humidity
+                  var sm = $("<small>");
+                  $(this).append("<div>");
+                  $(this).append(sm);
+                  $(this).append("<div>");
+                  var temcon= ((response.daily[counter].temp.max) - 273.15) * (9/5) + 32
+                  temround = Math.round(10*temcon)/10;
+                  sm.html("Temp: " + temround+"°F"+"<br>"+"Humidity: "+response.daily[counter].humidity+"%");                 
+                  counter++
+                })
+            }  
+        })
+    });
+}
 
-//on click function save the city name with unique id (maybe length of local storage)
-//loop through local storage after every enter button 
-//as well as beginning of rendering so every local storgae has a button with name of city in it
+
 //on click of city take the name of the city and complete an ajax call 
